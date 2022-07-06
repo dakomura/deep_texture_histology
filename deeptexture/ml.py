0097caeab30fa7a6c1b4b1c8458a77c328fc071b
Daisuke Komura <kdais-prm@m.u-tokyo.ac.jp>
2022-07-06 18:55:06 +0900

    bug fix

--- deeptexture/ml.py
+++ deeptexture/ml.py
@@ -45,18 +45,18 @@ class ML:
                           )
         df[y_name] = y
 
-        outstring = "s = cl.setup(df," \ 
-            "            target = y_name," \ 
-            "            fold_strategy = 'groupkfold'," \
-            "            fold_groups = cases," \
-            "            fold = fold," \
-            "            pca = pca," \
-            "            session_id = 123," \
-            "            )" \
-            "ml_model = cl.create_model(model)" \
-            "tuned_model = cl.tune_model(ml_model)" \
-            "pred = cl.predict_model(tuned_model," \
-            "                        verbose = True," \
+        outstring = "s = cl.setup(df," + \
+            "            target = y_name," + \
+            "            fold_strategy = 'groupkfold'," + \
+            "            fold_groups = cases," + \
+            "            fold = fold," + \
+            "            pca = pca," + \
+            "            session_id = 123," + \
+            "            )" + \
+            "ml_model = cl.create_model(model)" + \
+            "tuned_model = cl.tune_model(ml_model)" + \
+            "pred = cl.predict_model(tuned_model," + \
+            "                        verbose = True," + \
             "                        )"
         print(outstring)
         return tuned_model
