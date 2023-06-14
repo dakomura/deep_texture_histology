43d4396283dbb648336735d07a22bcb37e619758
Daisuke Komura <kdais-prm@m.u-tokyo.ac.jp>
2023-06-14 16:26:04 +0900

    multiple location fillter in search_multi

--- deeptexture/cbir.py
+++ deeptexture/cbir.py
@@ -441,8 +441,14 @@ class CBIR:
 
                 if fkey is not None:
                     v = self.df_attr[fkey][res]
-                    if not v in fval:
-                        continue
+                    if '/' in v: #multiple values in val
+                        vs = v.split('/')
+                        if np.sum([v_each in fval for v_each in vs]) == 0:
+                            continue
+                    else:
+                        if not v in fval:
+                            continue
+
                 if qkey is not None:
                     v = self.df_attr[qkey][res]
                     if v != qval:
